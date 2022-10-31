13e42476ae2d585bdc5b75ad8193d56d58aa3f08
Nazokat003 <117066993+Nazokat003@users.noreply.github.com>
2022-10-31 12:16:09 +0500

    Add files via upload

---
+++ 1.amaliy.py
@@ -0,0 +1,6 @@
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+s=2*(a*b+b*c+a*c)
+v=a*b*c
+print(f"yuzasi{s} hajmi{v}")
---
+++ 2.amaliy.py
@@ -0,0 +1,5 @@
+
+
+a=int(input("a="))
+v=a**3
+print(f"kub hajmi {v}")
